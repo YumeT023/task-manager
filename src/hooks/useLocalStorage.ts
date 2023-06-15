0ce23297f19a17767712456f9dbebb5afd6250d9
Yume <hei.hajatiana@gmail.com>
2023-06-15 10:21:31 +0300

    chore: add localStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,7 +1,8 @@
 const useLocalStorage = () => {
+  return {
+    read: (key: string) => localStorage.getItem(key),
+    save: (key: string, value: string) => localStorage.setItem(key, value),
+  };
+};
 
-}
-
-export {
-  useLocalStorage
-}
+export { useLocalStorage };
