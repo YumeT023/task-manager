6a804ed69bbb845143e326bf990c9bae08b0b480
Yume <hei.hajatiana@gmail.com>
2023-06-15 10:18:29 +0300

    feat: add task manager store

--- src/store/useTaskManager.ts
+++ src/store/useTaskManager.ts
@@ -1,7 +1,55 @@
-const useTaskManager = () => {
+import { create } from "zustand";
+import { Task } from "@/pages/tasks";
 
-}
+type State = {
+  tasks: Task[];
+  searchTask: string;
+};
 
-export {
-  useTaskManager
-}
+type Actions = {
+  setSearchTask(keyword: string): void;
+  addTask(task: Task): void;
+  updateTask(id: number, updated: Partial<Task>): void;
+  deleteTask(id: number): void;
+};
+
+const useTaskManager = create<State & Actions>((set, get) => ({
+  tasks: [],
+  searchTask: "",
+  addTask: (task: Task) => {
+    set({
+      tasks: [...get().tasks, task],
+    });
+  },
+  deleteTask(id: number) {
+    const tasks = get().tasks;
+
+    const updated = tasks.reduce(
+      (prev, current) => (current.id === id ? prev : [...prev, current]),
+      [] as Task[]
+    );
+
+    set({ tasks: updated });
+  },
+  updateTask(id: number, partialUpdate: Partial<Task>) {
+    const tasks = get().tasks;
+
+    const updated = tasks.reduce((prev, curr) => {
+      if (curr.id === id) {
+        curr = { ...curr, ...partialUpdate };
+      }
+      return [...prev, curr];
+    }, [] as Task[]);
+
+    set({
+      tasks: updated,
+    });
+  },
+  setSearchTask(searchTask: string) {
+    set({
+      searchTask,
+    });
+  },
+}));
+
+export { useTaskManager };
